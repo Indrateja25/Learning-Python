e39d094efcae6f5b10d0f8e35ee3bd71193ef0e2
Indra teja <itp10@iitbbs.ac.in>
2018-03-16 19:53:12 +0530

    if-else-added

---
+++ if-else.py
@@ -0,0 +1,9 @@
+age = 32
+if age < 18:
+    print("Hey kiddo.You are not elgible to vote & marry.\ngo and watch pogo")
+elif age >= 18 & age < 21:
+    print('Hey Bro.You are elgible to vote\nbut has to wait to marry!!')
+elif age >= 21 and age < 30:
+    print("Hey Dude.You are now officially given your chance  to ruin your life.\ndon't marry!! stay happy.")
+else:
+    print("Hey Uncle. How are the kids doing?")

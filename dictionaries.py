52ccc3beea9781d23f7eaa112a5be4fdf0683953
Indra teja <itp10@iitbbs.ac.in>
2018-03-17 12:12:07 +0530

    dictionaries and sets done

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+#dictionaries
+bestfriends =  {'humpty':'bumpty','chakri':'bush','it':'tya'}
+
+for k,v in bestfriends.items():
+    print(k,'<-->',v)
+
+#sets
+fuckers = {'it','tya','me','unknown shit'}
+for f in fuckers:
+    print(f)

c47b54e3699ffec4fe9cb9fecb466272a1964b37
Indra teja <itp10@iitbbs.ac.in>
2018-03-17 11:06:15 +0530

    loops-added

---
+++ loops.py
@@ -0,0 +1,8 @@
+proteins = ['eggs','fish','meat','beans','milk','yogurt','soya']
+for a in proteins:
+    print(a)
+    print(len(a))
+p = 0
+while  p in  range(len(proteins)):
+    print(p)
+    p = p+1
